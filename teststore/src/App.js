e065ed066716d7e0c7fbcb4b63ade37fedb57d4d
MJB129 <romedigital2.0@gmail.com>
2023-12-11 11:35:06 -0500

    changes

--- teststore/src/App.js
+++ teststore/src/App.js
@@ -1,21 +1,29 @@
+import React from 'react';
 import './App.css';
 import Button from './Components/Button/Button';
 import Card from './Components/Card/card';
 const { getData } = require("./Components/db/db");
+
 const bud = getData(); 
 
 function App() {
   return (
-    <>Welcome to Fifth Pocket Exotics
-    <Button title={'Add'} disable={false} type={'add'} />
-    <Button title={'Remove'} disable={false} type={'remove'} />
-    <Button title={'Purchase Now'} disable={false} type={'checkout'} /> 
-    {bud.map((bud) => (
-      <Card bud={bud} key={bud.id} />
-    ))}
-    </>
+    <React.Fragment>
+      <h1 className="title">Welcome to Fifth Pocket Exotics</h1>
+      Menu
+      <Button title={'Add'} disable={false} type={'add'} />
+      <Button title={'Remove'} disable={false} type={'remove'} />
+      <Button title={'Purchase Now'} disable={false} type={'checkout'} /> 
 
+      <div className="cards__container">
+        {bud.map((bud) => (
+          <Card bud={bud} key ={bud.id} />
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
+// Remove the closing curly brace
+// }
 
 export default App;
--- teststore/src/Components/db/db.js
+++ teststore/src/Components/db/db.js
@@ -1,14 +1,14 @@
 import accessoriesImg from "../images/accessories.jpg";
-import concentrates1Img from "../images/concentrates1.jpeg";
+import concentrates1Img from "../images/concentrates.jpeg";
 import edibles1Img from "../images/edibles1.jpg";
-import flowertestpic1Img from "../images/flowertestpic.jpg";
+import flowertestpicImg from "../images/flowertestpic.jpg";
 import shakeImg from "../images/shake.jpg";
 import shroomsImg from "../images/shrooms.jpg";
 import smallsImg from "../images/smalls.jpg";
 
 export function getData(){
     return [
-        {title:"Fifth Pocket", price: 75, img: flowertestpic1Img, desc: "The bud you keep in your fifth pocket."}
+        {title:"Fifth Pocket", price: 75, img: flowertestpicImg, desc: "The bud you keep in your fifth pocket.",id:1}
         ,{title:"Front Pocket", price: 40, img: smallsImg, desc: "The bud you share with the homies."},
         {title:"Back Pocket", price: 30, img: shakeImg, desc: "The bud you smoke when you're broke."},
         {title:"Edibles", price: 15, img: edibles1Img, desc: "The bud you eat."},
